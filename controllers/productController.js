93dc50bc573f5ea10022df6dfb08e1d2b30ca886
MikiyasWT <mikiywendu44@gmail.com>
2024-01-15 12:22:24 +0300

    seed data added

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,24 @@
+const Product = require("../models/product");
+
 // Get all products   =>   /api/v1/products?keyword=apple
 exports.getProducts = async (req, res, next) => {
+  const product = await Product.find();
+  if (product.status == 400) {
+    console.log(error);
+  }
   res.status(200).json({
     success: true,
     message: "no products in our stock",
+    product,
+  });
+};
+
+exports.newProduct = async (req, res, next) => {
+  console.log(req.body);
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
   });
 };
