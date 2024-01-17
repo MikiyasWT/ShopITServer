e89b9c70844b6ec5dd0a935c8be23f219e02d9e4
MikiyasWT <mikiywendu44@gmail.com>
2024-01-17 14:33:01 +0300

    custom error handler added

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(express.json());
+
+const errorMiddleware = require("./middlewares/errors");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Import all routes
 const products = require("./routes/product");
 
 app.use("/api/", products);
+
+//middlewares comes after routes
+//middleware
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ middlewares/errors.js
@@ -0,0 +1,24 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(err.statusCode || 500).json({
+      success: false,
+      message: err.message || "Internal Server Error",
+    });
+  }
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    console.log(err);
+
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+};
