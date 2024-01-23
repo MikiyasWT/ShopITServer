2524c5a613fa59d103899f8c024d3f6d81c4639a
MikiyasWT <mikiywendu44@gmail.com>
2024-01-23 11:28:41 +0300

    product search added

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,9 +2,32 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncError");
+const ApiFeatures = require("../utils/apiFeatures");
+const product = require("../models/product");
 
-// Get all products   =>   /api/v1/products?keyword=apple
-exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+// Create new product   =>   /api/admin/product/new
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  return res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+// Get all products   =>   /api/products?keyword=apple
+exports.getProduct = catchAsyncErrors(async (req, res, next) => {
+  const apiFeatures = new ApiFeatures(Product.find(), req.query).search();
+
+  products = await apiFeatures.query;
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+// Get all products (Admin)  =>   /api/admin/products
+exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
   try {
     const products = await Product.find();
     if (products.status == 400 || products === null) {
@@ -20,6 +43,8 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Internal Server Error", 500));
   }
 });
+
+//get a single product by ID api/product/:id
 
 exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   const _id = req.params.id;
@@ -43,15 +68,7 @@ exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
-exports.newProduct = catchAsyncErrors(async (req, res, next) => {
-  const product = await Product.create(req.body);
-
-  return res.status(201).json({
-    success: true,
-    product,
-  });
-});
-
+// Update Product   =>   /api/admin/product/:id
 exports.updateProductById = catchAsyncErrors(async (req, res, next) => {
   try {
     let product = await Product.findById(req.params.id);
@@ -80,6 +97,7 @@ exports.updateProductById = catchAsyncErrors(async (req, res, next) => {
   }
 });
 
+// Delete Product   =>   /api/admin/product/:id
 exports.deleteProductId = catchAsyncErrors(async (req, res, next) => {
   try {
     let product = await Product.findById(req.params.id);
--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getProducts,
+  getAdminProducts,
+  getProduct,
   getSingleProduct,
   newProduct,
   updateProductById,
   deleteProductId,
 } = require("../controllers/productController");
 
-router.route("/products").get(getProducts);
-
 router.route("/admin/product/new").post(newProduct);
+
+router.route("/admin/products").get(getAdminProducts);
+
+router.route("/product").get(getProduct);
 
 router.route("/product/:id").get(getSingleProduct);
 
